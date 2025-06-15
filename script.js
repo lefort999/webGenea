0a4a1a49a2272c24ce64bd0be090c29bc94d3c97
lefort999 <jlefort251@gmail.com>
2025-06-15 12:08:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function onChange() {
     output_text_element = document.getElementById("output-label");
     console.log("La profession sélectionnée est :", current_selected_item)
     output_text_element.value = current_selected_item;
+    if (current_selected_item === "Douanier") {
+        output_text_element.value = "C'est une belle profession"
+    }
+    else if (current_selected_item === "Boulanger") {
+        output_text_element.value = "Baguette"
+    }
 }
